3e2e70fce4ea00b2293d1174fd9367704f09b2fa
Jiwon Kang <jiwonk42@gmail.com>
2019-10-16 18:09:04 -0700

    fetch API data

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import './App.css';
 const App = () => {
   const APP_ID = "d5709585";
   const APP_KEY = "5f0e6246728c7016bad715bc3089d9bc";
-  const sampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes =async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text"/>
-        <button className="search-button" type="submit">{counter}</button>
+        <button className="search-button" type="submit">Search</button>
       </form>
     </div>
   );
