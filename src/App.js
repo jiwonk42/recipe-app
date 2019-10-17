5f2d4631d668a11d9cc6df75cc6949539a047f38
Jiwon Kang <jiwonk42@gmail.com>
2019-10-16 17:58:27 -0700

    set state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
   const APP_ID = "d5709585";
   const APP_KEY = "5f0e6246728c7016bad715bc3089d9bc";
   const sampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-  
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text"/>
-        <button className="search-button" type="submit">Search</button>
+        <button className="search-button" type="submit">{counter}</button>
       </form>
     </div>
   );
