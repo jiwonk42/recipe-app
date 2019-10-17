34fb7088af642533773fbf57298646d1ec67eebe
Jiwon Kang <jiwonk42@gmail.com>
2019-10-16 19:40:49 -0700

    add styling to .recipes

--- src/App.js
+++ src/App.js
@@ -36,15 +36,17 @@ const App = () => {
         <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">Search</button>
       </form>
-      {recipes.map((recipe, index) => (
-        <Recipe 
-          key={index} 
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories} 
-          image={recipe.recipe.image}
-          ingredients={recipe.recipe.ingredients}
-        />
-      ))}
+      <div className="recipes">
+        {recipes.map((recipe, index) => (
+          <Recipe 
+            key={index} 
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories} 
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
 };
