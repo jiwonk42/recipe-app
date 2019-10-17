dd5fa4ad234395859c41dbe3491838b8c589401d
Jiwon Kang <jiwonk42@gmail.com>
2019-10-16 18:32:47 -0700

    add updateSearch method

--- src/App.js
+++ src/App.js
@@ -7,22 +7,28 @@ const App = () => {
   const APP_KEY = "5f0e6246728c7016bad715bc3089d9bc";
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     getRecipes();
   }, []);
 
-  const getRecipes =async () => {
+  const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
   };
 
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+    console.log(search);
+  }
+
   return (
     <div className="App">
       <form className="search-form">
-        <input className="search-bar" type="text"/>
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">Search</button>
       </form>
       {recipes.map((recipe, index) => (
